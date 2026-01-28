5c5ed698c9fe998991cd86b85a681ead9ac236e4
helpdesk - pc <contatoluansantos7@gmail.com>
2026-01-28 15:21:24 -0300

    feat: debug env vars

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-
+console.log('API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
+console.log('Project ID:', process.env.REACT_APP_FIREBASE_PROJECT_ID);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
